ff7ae8415445b30371567c75e24ba2227947c6a7
nbegumc <60102360+nbegumc@users.noreply.github.com>
2020-02-15 13:25:44 +0100

    Add files via upload

---
+++ R/MovieDataAnalysis.R
@@ -0,0 +1,12 @@
+#'Movie Ratings Analysis
+#'
+#'
+#'Contains functions for both analyzing and visualizing movie ratings data
+#'
+#'
+#' @docType  package
+#'
+#' @author Nazli Begum Cirpanli \ e-mail {nazlibegum.cirpanli@studenti.unimi.it}
+#'
+#' @name MovieDataAnalysis
+NULL
---
+++ R/dataset.R
@@ -0,0 +1,27 @@
+#' List of Movies
+#'
+#' @docType data
+#'
+#' @usage data(movies)
+#'
+#' @format A data frame with 6 variables: \code{Films}, \code{Genre},
+#'   \code{CriticsRatings}, \code{AudienceRatings}, \code{BudgetinMillions},
+#'   \code{Year}
+#' \describe{This dataset consists of a list of movies for the years 2007 - 2011. It includes
+#' ratings, years of release, genres and total budgets for each of the movies}
+#'
+#' \item{Films}{The name of the movie}
+#' \item{Genre}{The genre of the movie}
+#' \item{CriticsRatings}{Rotten Tomatoes ratings in % }
+#' \item{AudienceRatings}{Audience rating of the film in %}
+#' \item{BudgetinMillions}{Budget of the film in millions}
+#' \item{Year}{Release year of the film}
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://www.superdatascience.com/}
+#'
+#' @examples
+
+"movies"
